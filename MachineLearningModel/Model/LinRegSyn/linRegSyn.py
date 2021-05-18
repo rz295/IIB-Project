a0407f6285ba87d7db18947e4d88303448ec15d5
rz295 <78133911+rz295@users.noreply.github.com>
2021-05-19 01:27:17 +0800

    Add files via upload

---
+++ MachineLearningModel/Model/LinRegSyn/linRegSyn.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 11 21:06:47 2021
+
+@author: raezhao
+"""
+
+#   This script trains synthetic BME680 Temp data ranging from -40degC to 85degC.
+#   Linear regression model is used.
+#   Raw Temp data are obtained by reversing the conversion routine from bme680.c file.
+import math
+import pandas as pd
+from sklearn import linear_model
+import glob
+
+calibratedTemp = pd.read_csv("/Users/raezhao/Desktop/IIB-Project/MachineLearningModel/Data/SyntheticTrainingData/SyntheticTempInt.csv") 
+testPath = r'/Users/raezhao/Desktop/IIB-Project/MachineLearningModel/Data/TestData2' 
+
+li2, rawADC, testTemp, trueTemp, Pi_1t, Pi_2t, Pi_3t, Pi_4t = ([] for i in range(8))
+
+#   Calibration parameter values from sensorCalibData.xlsx
+t1LSB = 0x04
+t1MSB = 0x67
+t2LSB = 0xC0
+t2MSB = 0x4E
+t3 = 0x66
+t1 = int((hex(t1MSB * (2**8))), 16) | int((hex(t1LSB)), 16)
+t2 = int((hex(t2MSB * (2**8))), 16) | int((hex(t2LSB)), 16)
+
+#   Extract BME680 Temp raw strings from csv files
+def read_csv(Path, li):
+    Files = glob.glob(Path + "/*.csv")
+    for filename in Files:
+        df = pd.read_csv(filename, index_col=None, header=0)
+        li.append(df)
+    frameTraining = pd.concat(li, axis=0, ignore_index=True)
+    d = pd.DataFrame(frameTraining, columns = [' BME680 Temp'])
+    return d
+
+#   Concatenate strings tempMSB, tempLSB and tempXLSB into temp_adc
+def concatenate_temp_adc(string, iterations):
+    rawTemp = string.iloc[iterations,].values
+    split=str.split(rawTemp[0])
+    tempMSB = int(split[0],16)
+    tempLSB = int(split[1],16)
+    tempXLSB = int(split[2],16)
+    temp_adc = int((hex((tempMSB & 0xFF) * (2**12))), 16) | int((hex((tempLSB & 0xFF) * (2**4))), 16) | int(hex((tempXLSB & 0xF0) // (2**4)), 16)
+    return temp_adc
+
+#   Adapted from conversion routine in BME680.c for temperature in integers
+def calib_temp(temp_adc):
+    var1 = (temp_adc / (2**3)) - (t1 * 2)
+    var2 = (var1 * t2) / (2**11)
+    var3 = ((((var1 / 2) * (var1 /2)) / (2**11)) * (t3 * (2**4))) / (2**14)
+    t_fine = var2 + var3
+    calc_temp = (((t_fine) * 5) + 128) / (2**8) / 100
+    return calc_temp
+
+d2 = read_csv(testPath, li2)
+
+#   Iterate over synthetic training data
+for i in range(0, 121):
+    t_fine = (calibratedTemp['0'][i] * 100 * (2**8) - 128) / 5
+    radicand = (t2**2) / (2**22) + t3 * t_fine / (2**21)
+    var1 = (-t2 / (2**11) + math.sqrt(radicand)) * (2**22) / t3
+    temp_ADC = (2**3) * (var1 + 2 * t1)
+    Pi_1t.append(calibratedTemp['0'][i]/(t1**2))
+    Pi_2t.append(t3)
+    Pi_3t.append(t1/t2)
+    Pi_4t.append(t1/temp_ADC)
+
+Pit_groups = {
+                'Pi_1t': Pi_1t,
+                'Pi_2t': Pi_2t,
+                'Pi_3t': Pi_3t,
+                'Pi_4t': Pi_4t       
+                }
+
+dg = pd.DataFrame(Pit_groups,columns=['Pi_1t', 'Pi_2t', 'Pi_3t', 'Pi_4t'])
+
+#   Here we have 4 variables for multiple regression. 
+#   If you just want to use one variable for simple linear regression, then use X = df['Interest_Rate'] for example.
+#   Alternatively, you may add additional variables within the brackets
+Xt = dg[['Pi_2t','Pi_3t','Pi_4t']] 
+Yt = dg['Pi_1t']
+
+#   Perform linear regression model with sklearn
+regr = linear_model.LinearRegression()
+regr.fit(Xt, Yt)
+
+print('Intercept: \n', regr.intercept_)
+print('Coefficients: \n', regr.coef_)
+
+#   Use the trained linear regression model on test data, calculate testTemp and later compare it with trueTemp
+Intercept = regr.intercept_
+Coeffs = [regr.coef_[0],  regr.coef_[1],  regr.coef_[2]]
+
+#   Iterate over test data
+for i in range(0, 45000):
+    #   Computed rawADC 
+    temp_test_adc = concatenate_temp_adc(d2, i)
+    rawADC.append(temp_test_adc)
+
+    #   Computed testTemp
+    Pi_2 = t3
+    Pi_3 = t1 / t2
+    Pi_4 = t1/temp_test_adc
+    modelEqn = Coeffs[0] * Pi_2 + Coeffs[1] * Pi_3 + Coeffs[2] * Pi_4 + Intercept
+    testTemp.append(modelEqn * (t1**2))
+
+    #   Computed trueTemp 
+    calc_temp_true = calib_temp(temp_test_adc)
+    trueTemp.append(calc_temp_true)
+
+dx = pd.DataFrame(testTemp, columns= None)
+dx.to_csv("/Users/raezhao/Desktop/IIB-Project/MachineLearningModel/Results/LinRegSyn22/TestValues.csv")
+dy = pd.DataFrame(trueTemp, columns= None)
+dy.to_csv("/Users/raezhao/Desktop/IIB-Project/MachineLearningModel/Results/LinRegSyn22/TrueValues.csv")
+dz = pd.DataFrame(rawADC, columns= None)
+dz.to_csv("/Users/raezhao/Desktop/IIB-Project/MachineLearningModel/Results/LinRegSyn22/rawADC.csv")
---
+++ MachineLearningModel/Model/LinRegSyn/percentError.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 25 16:40:10 2021
+
+@author: raezhao
+"""
+
+import pandas as pd 
+import statistics as stats
+import matplotlib.pyplot as plt
+
+predictedData = pd.read_csv("/Users/raezhao/Desktop/IIB-Project/MachineLearningModel/Results/LinRegSyn22/TestValues.csv") 
+trueData = pd.read_csv("/Users/raezhao/Desktop/IIB-Project/MachineLearningModel/Results/LinRegSyn22/TrueValues.csv") 
+rawADC = pd.read_csv("/Users/raezhao/Desktop/IIB-Project/MachineLearningModel/Results/LinRegSyn22/rawADC.csv") 
+errorPer = []
+
+for i in range(0, 45000):
+    error = (predictedData['0'][i] - trueData['0'][i]) / trueData['0'][i] * 100
+    errorPer.append(error)
+    
+print (stats.mean(errorPer))
+print (stats.stdev(errorPer))
+
+plot1 = plt.figure(1)
+plt.plot(rawADC['0'], predictedData['0'], label = "Predicted Data")
+plt.plot(rawADC['0'], trueData['0'], label = "True Data")
+plt.xlabel('Raw ADC Data (V)')
+#   Set the y axis label of the current axis.
+plt.ylabel('Calibrated Data ($^\circ$C)')
+#   Set a title of the current axes.
+plt.title('True and Predicted Calibrated Data VS Raw ADC Data')
+#   show a legend on the plot
+plt.legend()
+#   Display a figure.
+plt.show()
+
+plot2 = plt. figure(2)
+plt. plot(rawADC['0'], errorPer)
+plt.xlabel('Raw ADC Data (V)')
+#   Set the y axis label of the current axis.
+plt.ylabel('Percent Error (%)')
+#   Set a title of the current axes.
+plt.title('Percent Error VS Raw ADC Data')
+#   show a legend on the plot
+plt.legend()
+#   Display a figure.
+plt.show()
